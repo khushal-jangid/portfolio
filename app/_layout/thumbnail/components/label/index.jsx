c8cd4c1ee57c47cdc71579314c5406291f8a779a
Khushal Jangid <148229646+khushal-jangid@users.noreply.github.com>
2026-09-02 22:23:20 +0530

    fix(mobile): hide cursor label on mobile touch screens

--- app/_layout/thumbnail/components/label/index.jsx
+++ app/_layout/thumbnail/components/label/index.jsx
@@ -1,12 +1,32 @@
-/** @param {import('react').PropsWithChildren<unknown>} */
-export function ThumbnailLabel({ children }) {
-  return (
-    <div
-      style={{
-        paddingInlineStart: 'clamp(2.5em, 8vw, 8em)',
-      }}
-    >
-      <h5 className='text-xs text-secondary-foreground'>{children}</h5>
-    </div>
-  );
-}
+'use client';
+
+import { forwardRef } from 'react';
+
+import { motion } from 'framer-motion';
+
+import { Center } from '@/components';
+
+import { CursorLabel } from './index.styled';
+
+const MotionComponent = motion(CursorLabel);
+
+export const ThumbnailCursorLabel = forwardRef(
+  /**
+   * @param {import('react').HTMLAttributes<HTMLElement> & { variants: import('framer-motion').Variants; active: boolean;}} props
+   * @param {import('react').ForwardedRef<HTMLElement>} ref
+   */
+  function ThumbnailCursorLabel({ variants, active, ...props }, ref) {
+    return (
+      <MotionComponent
+        ref={ref}
+        className='pointer-events-none fixed left-1/2 top-1/2 hidden md:flex items-center justify-center text-sm font-semibold'
+        variants={variants}
+        initial='initial'
+        animate={active ? 'enter' : 'closed'}
+        {...props}
+      />
+    );
+  },
+);
+
+ThumbnailCursorLabel.displayName = 'ThumbnailCursorLabel';
